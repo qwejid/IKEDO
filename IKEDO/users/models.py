9c02967ad86945d3bf83acf3af75bb7e7871787f
Kristina Shishkina <klikklok2017@mail.ru>
2023-11-19 21:10:12 +0300

    создание супер пользователя корекировки

--- IKEDO/users/models.py
+++ IKEDO/users/models.py
@@ -16,10 +16,11 @@ class CustomUserManager(UserManager):
         password: str,                
         commit: bool,
         is_staff: bool = False, 
-        is_superuser: bool = False,        
+        is_superuser: bool = False,  
+        token: str = None,      
     ):         
         email = self._get_email(email)         
-        user = User(first_name=first_name, last_name=last_name, email=email, username=email, is_staff=is_staff, is_superuser=is_superuser)
+        user = User(first_name=first_name, last_name=last_name, email=email, username=email, is_staff=is_staff, is_superuser=is_superuser, token=token)
         user.set_password(password)
          
         if commit:
@@ -27,8 +28,8 @@ class CustomUserManager(UserManager):
              
         return user
  
-    def create_superuser(self, first_name: str, last_name: str, email: str, password: str, commit: bool = True):
-        return self._create_user(first_name, last_name, email, password, is_staff=True, is_superuser=True, commit=commit)
+    def create_superuser(self, first_name: str, last_name: str, email: str, password: str, token: str = None, commit: bool = True):
+        return self._create_user(first_name, last_name, email, password, is_staff=True, is_superuser=True, token=token, commit=commit)
  
     def create_user(self, first_name: str, last_name: str, email: str, password: str, commit: bool = True):
         return self._create_user(first_name, last_name, email, password, commit=commit)
@@ -44,3 +45,9 @@ class User(AbstractUser):
     objects = CustomUserManager()
     
     
+
+# мой
+# eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1ZGJjOWUwZC0zNDg2LTRkNTAtOWYyYy0yMTYxZjc2ZWY0NzUiLCJnaXZlbl9uYW1lIjoi0JzQuNGF0LDQuNC7IiwiZmFtaWx5X25hbWUiOiLQqNC40YjQutC40L0iLCJ2ZXJzaW9uIjoidjEiLCJtaWRkbGVfbmFtZSI6ItCV0LLQs9C10L3RjNC10LLQuNGHIiwidG9rZW5fdHlwZSI6IjMiLCJuYmYiOjE3MDAxMjcwNzAsImV4cCI6MTczMTc0NzM1MCwiaXNzIjoiYXBpLWd3LmtlZG8tZGVtby5jbG91ZC5hc3RyYWwtZGV2LnJ1IiwiYXVkIjoia2VkbyJ9.7Urqx-_Ht_ybtOZ9wfIDgzX1qy7_wYa7_sV347WLuME
+
+# дима
+# eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJjNjllOWIwYS0xYzg0LTQ0YmYtYTRmYy01OTY4NGM3ZDMyZTciLCJnaXZlbl9uYW1lIjoi0JDQu9C10LrRgdC10LkiLCJmYW1pbHlfbmFtZSI6ItCY0LLQsNC90L7QsiIsInZlcnNpb24iOiJ2MSIsIm1pZGRsZV9uYW1lIjoi0JDQvdC00YDQtdC10LLQuNGHIiwidG9rZW5fdHlwZSI6IjMiLCJuYmYiOjE3MDAzOTU2MjcsImV4cCI6MTczMjAxNzk3MywiaXNzIjoiYXBpLWd3LmtlZG8tZGVtby5jbG91ZC5hc3RyYWwtZGV2LnJ1IiwiYXVkIjoia2VkbyJ9.qlrbvVxd75hkR-wQ5dgqIeb-MNtvgZWgMiNX_kawUzg
